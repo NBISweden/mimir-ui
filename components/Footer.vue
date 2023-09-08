0b15585c38da46efbd55499136117eb36cca5374
jonandernovella <novella.uu@gmail.com>
2023-09-08 09:35:41 +0200

    chore: minor styling updates

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,21 +1,28 @@
 <template>
+<a href="https://nbis.se">
     <footer class="nbisfooter">
       <img class="logo" src="/nbislogo-green.svg" alt="NBIS logo">
-      <span>Mimir is a service maintained by the National Bioinformatics Infrastructure Sweden</span>
+      <div>National Bioinformatics Infrastructure Sweden</div>
     </footer>
+</a>
 </template>
 
 <style scoped>
   .nbisfooter {
     display: flex;
     flex-direction: column;
-    gap: 1rem;
+    flex-shrink: 2;
+    gap: 0.5rem;
     align-items: center;
     background-color: inherit;
     text-align: center;
   }
   .logo {
-    width: 150px;
+    width: 100px;
     height: auto;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
   }
   </style>
